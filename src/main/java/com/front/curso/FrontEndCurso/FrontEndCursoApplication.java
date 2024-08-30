7ed6c48d6d765689b8b502b109d11d75ac72f27b
Thiago <thiagoalves060@gmail.com>
2024-08-30 09:12:31 -0300

    first commit

---
+++ src/main/java/com/front/curso/FrontEndCurso/FrontEndCursoApplication.java
@@ -0,0 +1,13 @@
+package com.front.curso.FrontEndCurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontEndCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontEndCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/front/curso/FrontEndCurso/FrontEndCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.front.curso.FrontEndCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontEndCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
